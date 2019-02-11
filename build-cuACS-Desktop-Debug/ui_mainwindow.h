c2d9d58242fb3152e9d0320f6d8422d264a18208
Hussein Khlawey <0hussein.k@gmail.com>
2019-02-10 19:07:40 -0500

    added basic sql functionality

---
+++ build-cuACS-Desktop-Debug/ui_mainwindow.h
@@ -0,0 +1,83 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.5
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QMenuBar *menuBar;
+    QMenu *menucuACS;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(316, 576);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(20, 400, 91, 25));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 316, 22));
+        menucuACS = new QMenu(menuBar);
+        menucuACS->setObjectName(QStringLiteral("menucuACS"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menucuACS->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "View Animal", Q_NULLPTR));
+        menucuACS->setTitle(QApplication::translate("MainWindow", "cuACS", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
--- cuACS/main.cpp
+++ cuACS/main.cpp
@@ -1,11 +1,73 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QtDebug>
+#include <QtSql>
+
+void runQuery(QString input){
+    QSqlQuery query;
+    query.prepare(input);
+
+    if(!query.exec()){
+        qDebug() << "Can't run query";
+    }
+    else{
+        qDebug() << "Ran query";
+    }
+}
+
+void addValues(int id, QString name, int gender, QString species, QString breed, int age, QString weight, QString height, QString colour){
+    QSqlQuery qry;
+    qry.prepare("INSERT OR REPLACE INTO animals VALUES (?,?,?,?,?,?,?,?,?);");
+
+    qry.addBindValue(id);
+    qry.addBindValue(name);
+    qry.addBindValue(gender);
+    qry.addBindValue(species);
+    qry.addBindValue(breed);
+    qry.addBindValue(age);
+    qry.addBindValue(weight);
+    qry.addBindValue(height);
+    qry.addBindValue(colour);
+
+    if(!qry.exec()){
+        qDebug() << "Can't add values";
+    }
+    else{
+        qDebug() <<"Added values";
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
 
+    qDebug() << "Starting";
+
+    QSqlDatabase myDatabase = QSqlDatabase::addDatabase("QSQLITE");
+    myDatabase.setDatabaseName("/home/student/Desktop/NullPointerException/cuACS/myDatabase");
+
+    if(!myDatabase.open()){
+            qDebug() << "Can't open database";
+        }
+        else{
+            qDebug()<<"Opened database";
+        }
+
+    QString currentQuery = "DROP TABLE IF EXISTS animals;";
+    runQuery(currentQuery);
+
+    //gender: 0 is female, 1 is male
+    currentQuery = "CREATE TABLE IF NOT EXISTS animals (id INTEGER PRIMARY KEY,"
+                   "name TEXT, gender INTEGER, species TEXT, breed TEXT, age INTEGER,"
+                   "weight INTEGER, height INTEGER, colour TEXT);";
+    runQuery(currentQuery);
+
+    //adding values to database
+    addValues(0,"Bloo",1,"dog","rottweiler",7,"55","2.5","black");
+
+    qDebug() << "Ending";
+    myDatabase.close();
+    w.show();
     return a.exec();
 }
